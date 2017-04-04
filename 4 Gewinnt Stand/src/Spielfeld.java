a4abd5e6a971ecac369d7ed3305850166ab9de56
alexBckl <alexander.boeckle@gmail.com>
2017-04-04 15:36:28 +0200

    Update Spielfeld

--- 4 Gewinnt Stand/src/Spielfeld.java
+++ 4 Gewinnt Stand/src/Spielfeld.java
@@ -10,9 +10,9 @@ public class Spielfeld {
 	
 	//Operationen
 	public void initialisiere() {
-		for(int x = 0; x < 7; x++) {
+		for(int x = 0; x < 8; x++) {
 			for(int y = 0; y < 8; y++) {
-				feld[x][y] = 1;
+				feld[x][y] = 0;
 			}
 		}
 	}
